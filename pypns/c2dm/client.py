69bf9ed5a64b14959d425136cd8998ab17314061
ggarber <gustavogb@gmail.com>
2012-04-16 23:15:33 +0200

    c2dm functional implementation

--- pypns/c2dm/client.py
+++ pypns/c2dm/client.py
@@ -1,85 +1,155 @@
+import uuid
 import urllib
 from twisted.python import log
 from twisted.internet import reactor
+from twisted.internet import defer
+from twisted.internet.protocol import Protocol
 from twisted.application import service
 from twisted.web.client import Agent
 from twisted.web.http_headers import Headers
+from twisted.web.iweb import IBodyProducer
 from zope.interface import implements
-from pypns.base import IPNSService, register_factory
+from pypns.base import IPNSService
 
 CLIENT_LOGIN_URL = 'https://www.google.com/accounts/ClientLogin'
 C2DM_URL = 'https://android.apis.google.com/c2dm/send'
+
+class BufferProtocol(Protocol):
+    def __init__(self):
+        self._buffer = ''
+        self.done = defer.Deferred()
+
+    def connectionMade(self):
+        pass
+
+    def dataReceived(self, bytes):
+        self._buffer += bytes
+
+    def connectionLost(self, reason):
+        self.done.callback(self._buffer)
+
+class BufferProducer(object):
+    implements(IBodyProducer)
+
+    def __init__(self, body):
+        self.body = body
+        self.length = len(body)
+
+    def startProducing(self, consumer):
+        consumer.write(self.body)
+        return defer.succeed(None)
+
+    def pauseProducing(self):
+        pass
+
+    def resumeProducing(self):
+        pass
+
+    def stopProducing(self):
+        pass
 
 class C2DMService(service.Service):
     """
     A Service that sends notifications to the C2DM Service
     """
-
     implements(IPNSService)
 
-    def __init__(self, email, passwd, environment, timeout=15):
+    def __init__(self, email, password, environment, timeout=15):
         log.msg('C2DMService __init__')
         self.agent = Agent(reactor)
         self.token = None
         self.environment = environment
         self.email = email
-        self.passwd = passwd
+        self.password = password
         self.timeout = timeout
 
+    @defer.inlineCallbacks
     def notify(self, registration_id, payload):
         """
         Connect to the C2DM service and send notifications
         """
+        log.msg('notify %s' % registration_id)
+
+        #TODO: Avoid making multiple get_token requests in parallel until you get the token
         if not self.token:
-            return self.get_token().addCallback(lambda: self.send_notify(registration_id, payload))
+            self.token = yield self.get_token()
 
-        return self.send_notify(registration_id, payload)
+        result = yield self.send_notify(registration_id, payload)
+        defer.returnValue(result)
 
+    @defer.inlineCallbacks
     def send_notify(self, registration_id, payload):
         log.msg('C2DMService.send_notify %s' % registration_id)
 
         values = {
-            'collapse_key' : '',
+            'collapse_key' : str(uuid.uuid1()),
             'registration_id' : registration_id,
             }
-        d = self.agent.request(
+        for k,v in payload.iteritems():
+            values['data.%s' % k] = v
+
+        response = yield self.agent.request(
             'POST',
             C2DM_URL,
             Headers({
-                'Authorization': 'GoogleLogin auth=' + self.token,
-                'Content-Type': 'application/x-www-form-urlencoded'}),
-            urllib.urlencode(values))
-        d.addErrback(log_errback('c2dm-service-write'))
-        return d
+                'Authorization': ['GoogleLogin auth=' + self.token],
+                'Content-Type': ['application/x-www-form-urlencoded']}),
+            BufferProducer(urllib.urlencode(values)))
 
+        protocol = BufferProtocol()
+        response.deliverBody(protocol)
+
+        response_content = yield protocol.done
+
+        responseAsList = response_content.split('\n')
+        key, val = responseAsList[0].split('=')
+
+        if key == 'Error':
+            raise Exception('Error sending notification ' + val)
+
+        defer.returnValue(val)
+
+    @defer.inlineCallbacks
     def get_token(self):
         log.msg('C2DMService.get_token')
 
         values = {
             'accountType' : 'HOSTED_OR_GOOGLE',
             'Email' : self.email,
-            'Passwd' : self.passwd,
+            'Passwd' : self.password,
             'source' : 'C2DMVALIDACCOUNT-C2DM-1',
-            'service' : 'c2dm'
+            'service' : 'ac2dm'
         }
-        d = self.agent.request(
+
+        response = yield self.agent.request(
             'POST',
             CLIENT_LOGIN_URL,
             Headers({
-                'Content-Type': 'application/x-www-form-urlencoded'
+                'Content-Type': ['application/x-www-form-urlencoded']
             }),
-            urllib.urlencode(values))
-        d.addCallback(self.parse_token)
-        d.addErrback(log_errback('c2dm-service-write'))
-        return d
+            BufferProducer(urllib.urlencode(values)))
 
+        token = yield self.parse_token(response)
+
+        log.msg('token %s' % token)
+
+        defer.returnValue(token)
+
+    @defer.inlineCallbacks
     def parse_token(self, response):
-        return response.deliverBody(BeginningPrinter(finished))
+        log.msg('parse_token')
 
-        responseAsList = response.split('\n')
-        self._token = responseAsList[2].split('=')[1]
-        self.token =  None
-        return response
+        protocol = BufferProtocol()
+        response.deliverBody(protocol)
+
+        response_content = yield protocol.done
+
+        log.msg('parse_token ' + response_content)
+
+        responseAsList = response_content.split('\n')
+        token = responseAsList[2].split('=')[1]
+
+        defer.returnValue(token)
 
 def log_errback(name):
     def _log_errback(err, *args):
